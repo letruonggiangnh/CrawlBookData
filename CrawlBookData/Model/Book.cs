b8419c34907d6c141e304d79aa074e71a0b48770
Giang Le <81180883+letruonggiangnh@users.noreply.github.com>
2024-01-26 21:13:55 +0700

    Initial commit

---
+++ CrawlBookData/Model/Book.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrawlBookData.Model
+{
+    public class Book
+    {
+        public Book(string BookName, string BookImageUrl, string BookPrice, string Writer, string Publisher, string CoverType) 
+        {
+            this.BookName = BookName;
+            this.BookImageUrl = BookImageUrl;
+            this.BookPrice = BookPrice;
+            this.Writer = Writer;
+            this.Publisher = Publisher;
+            this.CoverType = CoverType;
+        }
+        public string BookName { get; set; }
+        public string BookImageUrl { get; set; }
+        public string BookPrice { get; set; }
+        public string Writer { get; set; }
+        public string Publisher { get; set; }
+        public string CoverType { get; set; }
+    }
+}
--- CrawlBookData/Program.cs
+++ CrawlBookData/Program.cs
@@ -1,10 +1,51 @@
-﻿namespace CrawlBookData
+﻿using CrawlBookData.Model;
+using HtmlAgilityPack;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CrawlBookData
 {
     internal class Program
     {
-        static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            await DownloadSachVanHocAsync();
+            Console.WriteLine("Done");
+        }
+
+        public static async Task DownloadSachVanHocAsync() 
+        {
+            var web = new HtmlWeb();
+            var listBook = new List<Book>();
+            for (int i = 1; i <= 257; i++)
+            {
+                var document = await web.LoadFromWebAsync("https://www.fahasa.com/sach-trong-nuoc/van-hoc-trong-nuoc.html?order=num_orders&limit=24&p=" + i);
+                var productHTMLElements = document.DocumentNode.QuerySelectorAll("div.ma-box-content");
+                foreach (var htmlElement in productHTMLElements)
+                {
+                    string productDetailUrl = htmlElement.QuerySelector("div.images-container a").Attributes["href"].Value;
+                    var productDetailDocument = await web.LoadFromWebAsync(productDetailUrl);
+                    if (productDetailDocument.DocumentNode.QuerySelector("div.product-view-sa .product-view-sa_one .product-view-sa-supplier a") != null
+                         && productDetailDocument.DocumentNode.QuerySelector("div.product-view-sa_one .product-view-sa-author  span:nth-child(2)") != null
+                             && productDetailDocument.DocumentNode.QuerySelector("div.product-view-sa_two .product-view-sa-author  span:nth-child(2)") != null
+                                && productDetailDocument.DocumentNode.QuerySelector("div.price-box .price") != null
+                                    && productDetailDocument.DocumentNode.QuerySelector("div.product-essential-detail h1") != null
+                                        && productDetailDocument.DocumentNode.QuerySelector("div.product-view-image-product img") != null)
+                    {
+
+                        string bookName = productDetailDocument.DocumentNode.QuerySelector("div.product-essential-detail h1").InnerText;
+                        string bookNameFormatted = bookName.Trim();
+                        string price = productDetailDocument.DocumentNode.QuerySelector("div.price-box .price").InnerText;
+                        string supplier = productDetailDocument.DocumentNode.QuerySelector("div.product-view-sa_one .product-view-sa-supplier a").InnerText;
+                        string writer = productDetailDocument.DocumentNode.QuerySelector("div.product-view-sa_one .product-view-sa-author  span:nth-child(2)").InnerText;
+                        string coverType = productDetailDocument.DocumentNode.QuerySelector("div.product-view-sa_two .product-view-sa-author  span:nth-child(2)").InnerText;
+                        string imgUrl = productDetailDocument.DocumentNode.QuerySelector("div.product-view-image-product img").Attributes["data-src"].Value;
+                        Book book = new Book(bookNameFormatted, imgUrl, price, writer, supplier, coverType);
+                        listBook.Add(book);
+                        Console.WriteLine("Added");
+                    }
+                }
+            }
         }
     }
 }
